7be8d564496ee9db928b8a9403b62d44f1c2dd95
sumpan <dong_bin86@163.com>
2016-11-04 16:55:44 +0800

    添加聚合原理注释

--- src/main/java/mamba/aggregators/AbstractAggregator.java
+++ src/main/java/mamba/aggregators/AbstractAggregator.java
@@ -69,7 +69,10 @@ public abstract class AbstractAggregator implements MetricAggregator {
         this.outputTableName = outputTableName;
         this.nativeTimeRangeDelay = nativeTimeRangeDelay;
     }
-
+    /**
+     * @referenceTime 上一次的checkpoint时间
+     * @aggregatorPeriod checkpoint周期
+     * */
     public static long getRoundedCheckPointTimeMillis(long referenceTime, long aggregatorPeriod) {
         return referenceTime - (referenceTime % aggregatorPeriod);
     }
@@ -79,9 +82,13 @@ public abstract class AbstractAggregator implements MetricAggregator {
         return currentTime - (currentTime % aggregatorPeriod);
     }
 
+    /**
+     * 假设现成调度周期是100s，那么这里得到的时间相当月当前时间戳去掉后两位的零头
+     */
+
     @Override
     public void run() {
-        LOG.info("Started Timeline aggregator thread @ " + new Date());
+        LOG.info("Started Metrics aggregator thread @ " + new Date());
         Long SLEEP_INTERVAL = getSleepIntervalMillis();
         runOnce(SLEEP_INTERVAL);
     }
@@ -109,7 +116,18 @@ public abstract class AbstractAggregator implements MetricAggregator {
                             "previous checkpoint.");
                 }
             }
-        }
+        }/**虽然是根据周期调度，但是这里要通过checkpoint时间，决定要不要做聚合，因为太频繁的聚合没有意义，比如分钟级别的聚合，20秒又过来调度了
+         要不要做聚合，其实不需要
+
+         那么聚合的时候呢，还需要有一个起始时间和终止时间，还要解决两次聚合操作之间重复聚合某一段数据的问题
+
+
+         那么这里呢，lastCheckPointTime就是要聚合的数据的开始时间，聚合多久的数据呢，就是这个调度周期，如果没问题的话，下次聚合时间就是
+         lastCheckPointTime + SLEEP_INTERVAL
+
+         doWork的过程呢，基本上就是拼装sql然后，读出来结果，把一段时间内的数据全部捞进来，然后聚合，聚合玩完之后全部写入到聚合表里，这样相当于
+         离线异步完成聚合，那么查询的效率就会大大提升。
+         */
     }
 
     private long readLastCheckpointSavingOnFirstRun(long currentTime) {
@@ -117,12 +135,17 @@ public abstract class AbstractAggregator implements MetricAggregator {
 
         try {
             lastCheckPointTime = readCheckPoint();
+            /**
+             * 1.checkpoint目录保存的是上一次做checkpoint的时间信息，时间戳，解析出来这个上次的时间戳
+             * */
             if (lastCheckPointTime != -1) {
                 LOG.info("Last Checkpoint read : " + new Date(lastCheckPointTime));
                 if (isLastCheckPointTooOld(currentTime, lastCheckPointTime)) {
                     LOG.warn("Last Checkpoint is too old, discarding last checkpoint. " +
                             "lastCheckPointTime = " + new Date(lastCheckPointTime));
                     lastCheckPointTime = getRoundedAggregateTimeMillis(getSleepIntervalMillis()) - getSleepIntervalMillis();
+
+                    /**得到一个接近理论上上一个周期的checkpoint时间*/
                     LOG.info("Saving checkpoint time. " + new Date((lastCheckPointTime)));
                     saveCheckPoint(lastCheckPointTime);
 
@@ -131,7 +154,7 @@ public abstract class AbstractAggregator implements MetricAggregator {
                     if (lastCheckPointTime > 0) {
                         lastCheckPointTime = getRoundedCheckPointTimeMillis(lastCheckPointTime, getSleepIntervalMillis());
                         LOG.info("Rounded off checkpoint : " + new Date(lastCheckPointTime));
-                    }
+                    }/**根据当前的checkpoint周期，重启判断上次的checkpoint时间是否合理，如果距离上次时间太短了，那么这一轮就不做checkpoint*/
 
                     if (isLastCheckPointTooYoung(lastCheckPointTime)) {
                         LOG.info("Last checkpoint too recent for aggregation. Sleeping for 1 cycle.");
@@ -298,5 +321,16 @@ public abstract class AbstractAggregator implements MetricAggregator {
 
     protected String getCheckpointLocation() {
         return checkpointLocation;
+    }
+
+
+    public static void main(String[] args) {
+        long aggregatorPeriod = 100L;
+
+        long currentTime = System.currentTimeMillis();
+        System.out.println(currentTime);
+
+        System.out.println(currentTime % aggregatorPeriod);
+        System.out.println(currentTime - (currentTime % aggregatorPeriod));
     }
 }
